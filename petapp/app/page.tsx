4a021bed4fd8c5931e9056804febd42b9ab6f30f
nikolagsiderov <nikolagsiderov@gmail.com>
2024-02-12 20:17:24 +0200

    Trying to resolve pre-rendering...

--- petapp/app/page.tsx
+++ petapp/app/page.tsx
@@ -5,6 +5,8 @@ import getListings, { IListingsParams } from "@/app/actions/getListings";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "@/app/components/ClientOnly";
 
+export const dynamic = "force-dynamic";
+
 interface HomeProps {
   searchParams: IListingsParams;
 }
