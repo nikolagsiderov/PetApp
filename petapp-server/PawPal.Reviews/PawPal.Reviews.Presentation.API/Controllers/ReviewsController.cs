273be053a2c1ab32444d88872a7a10698d771e44
cholaka <cholakovge@gmail.com>
2024-12-02 20:22:15 +0200

    Reviews microservice initial setup

---
+++ petapp-server/PawPal.Reviews/PawPal.Reviews.Presentation.API/Controllers/ReviewsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PawPal.Reviews.Presentation.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReviewsController : ControllerBase
+    {
+    }
+}
