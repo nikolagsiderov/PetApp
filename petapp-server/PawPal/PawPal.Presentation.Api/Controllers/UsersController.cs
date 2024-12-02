b4707ca8a7b7cac2fc3d1ded60496e48a810b1b8
Georgi Cholakov <cholakovge@gmail.com>
2024-12-02 18:25:43 +0200

    PR 1: Setup PawPal.Users microservice architecture and project references (#1)
    * Setup PawPal.Users microservice architecture and project references
    
    * Commented out http file

---
+++ petapp-server/PawPal/PawPal.Presentation.Api/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PawPal.Presentation.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+    }
+}
