2481e6a105fe75bf6b7ece64da3bfb00da91f3b2
nikolagsiderov <nikolagsiderov@gmail.com>
2024-12-04 09:39:31 +0200

    Added reservations requests counters in the manage layout navigation menu option

--- petapp-web/app/(manager)/layout.tsx
+++ petapp-web/app/(manager)/layout.tsx
@@ -1,6 +1,7 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "../components/ClientOnly";
 import Sidebar from "../components/navbar/manage/Sidebar";
+import { getReservationRequests } from "@/app/actions/reservations/getActions";
 
 export default async function RootLayout({
   children,
@@ -9,9 +10,14 @@ export default async function RootLayout({
 }>) {
   const currentUser = await getCurrentUser();
 
+  const reservationRequests = await getReservationRequests();
+  const awaitingApproval = reservationRequests.filter(
+    (request) => request.approved === false
+  );
+
   return (
     <ClientOnly>
-      <Sidebar currentUser={currentUser} />
+      <Sidebar currentUser={currentUser} requestsCount={awaitingApproval.length} />
       <div>{children}</div>
     </ClientOnly>
   );
